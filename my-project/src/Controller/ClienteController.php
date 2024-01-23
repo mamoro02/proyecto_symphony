c4d976558eeea1a89fbeb3ed47d7173fb69f5a7e
mery <mamoro02@floridaoberta.es>
2024-01-23 09:05:01 +0100

    cambios

--- my-project/src/Controller/ClienteController.php
+++ my-project/src/Controller/ClienteController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Product;
-use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Cliente;
 use App\Entity\Emp;
@@ -55,8 +53,6 @@ class ClienteController extends AbstractController
             "totalPages" => $totalPages
         ]);
     }
-
-
 
 
     #[Route('/detail/{id}', name: 'client_show')]
